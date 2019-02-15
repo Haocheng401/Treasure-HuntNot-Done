ca9eb9fdb6f5c644b9e4dff7d54dedfe6c896cef
Haocheng401 <44624482+Haocheng401@users.noreply.github.com>
2019-02-14 16:28:18 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$("#img1").click(function(){
+    $("#text").text("Double click me");
+    $("#text").css("fontSize", "50px");
+    $("#img1").fadeToggle();
+    $("#img2").show();
+});
+
+$("#img2").dblclick(function(){
+    $("#text").text("Hover Over The Picture");
+    $("#img2").fadeToggle();
+    $("#img3").show();
+});
